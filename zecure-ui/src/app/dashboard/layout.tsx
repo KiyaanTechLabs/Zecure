d67324f010bc676f6cd4ec5a495ac583b71fcaf1
[Samarth Patil] <143065657+samarth3101@users.noreply.github.com>
2025-06-24 11:31:16 +0530

    Dashboard UI edits

--- zecure-ui/src/app/dashboard/layout.tsx
+++ zecure-ui/src/app/dashboard/layout.tsx
@@ -1,15 +1,15 @@
 // app/dashboard/layout.tsx
+"use client";
 import Sidebar from '@/components/Sidebar';
-import Navbar from '@/components/Navbar';
 import '@/styles/globals.scss';
+import styles from '@/styles/Dashboard.module.scss';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="dashboardLayout">
+    <div className={styles.dashboard}>
       <Sidebar />
-      <div className="dashboardMain">
-        <Navbar />
-        <div className="dashboardContent">{children}</div>
+      <div className={styles.main}>
+        <div className={styles.content}>{children}</div>
       </div>
     </div>
   );
